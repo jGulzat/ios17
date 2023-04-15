c2b8dcbc2023d1f8c0925aa9c2eb17769ed9ee73
gulzatzh <gulzatj97@mail.ru>
2023-04-15 18:13:10 +0600

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Gulzat on 15/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
